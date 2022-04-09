b71f42aad80aa80c28249d39b9f66bc1692a3b38
MorayYesilgueller <82274230+MorayYesilgueller@users.noreply.github.com>
2022-04-09 19:25:38 +0200

    finally fixed merge sort with 2d arrays

--- code/Main_Sort.py
+++ code/Main_Sort.py
@@ -1,4 +1,5 @@
 import random
+from socket import create_server
 import pygame
 import numpy
 
@@ -19,23 +20,25 @@ black= pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
 red = pygame.Color(255,0,0)
 
+
+#draws all lines in given array
 def draw_all(array):
     for i in range(len(array)):
         pygame.draw.line(screen,white,(50+2*(array[i,1]),10),(50+2*(array[i,1]),1000))
-        pygame.draw.line(screen,black,(50+2*(array[i,1]),10),(50+2*(array[i,1]),array[i,0]))
+        pygame.draw.line(screen,black,(50+2*(array[i,1]),10),(50+2*(array[i,1]),50+array[i,0]))
         pygame.display.update()
 
 
-#what in gods name is wrong
+#draws two lines from given array
 def draw_single(array,i,j):
     pygame.draw.line(screen,white,(50+2*i,10),(50+2*i,1000))
     pygame.draw.line(screen,white,(50+2*(j),10),(50+2*(j),1000))
-    pygame.draw.line(screen,black,(50+2*(j),10),(50+2*(j),array[i+1]))
-    pygame.draw.line(screen,black,(50+2*i,10),(50+2*i,array[i]))
+    pygame.draw.line(screen,black,(50+2*(j),10),(50+2*(j),50+array[i+1,0]))
+    pygame.draw.line(screen,black,(50+2*i,10),(50+2*i,50+array[i,0]))
     pygame.display.update()
 
 
-#Bubble sort algorythm
+#Bubble sort algorythm simply 
 def BumbleSortus(array):
     length = len(array)
     draw_all(array)
@@ -45,12 +48,12 @@ def BumbleSortus(array):
 
 
         for j in range(length-i-1):
-            if array[j]> array[j+1]:
-                array[j],array[j+1] = array[j+1], array[j]
-                draw_single(array,j,(j+1))
+            if array[j,0]> array[j+1,0]:
+                array[j,0],array[j+1,0] = array[j+1,0], array[j,0]
+                draw_single(array,array[j,1],array[j+1,1])
                 is_sorted = False
         
-            if array[j] == array [j+1]:
+            if array[j,0] == array [j+1,0]:
                 is_sorted = False
         if is_sorted:
             break
@@ -61,65 +64,70 @@ def BumbleSortus(array):
 #merging of two arrays using 2d arrays now 
 def merger(lhalf,rhalf):
 
-    arrayus= numpy.array([[0,0]])
+    array= numpy.array([[0,0]])
     il = ir= 0
     row_track= lhalf[0,1]
     counter = 1
 
-    while numpy.size(arrayus)-2 < numpy.size(lhalf) + numpy.size(rhalf):
 
+    #loop while halves not merged yet
+    while numpy.size(array)-2 < numpy.size(lhalf) + numpy.size(rhalf):
+
+
+        #comparison for elements in both halves based on index ir and il
         if int(lhalf[il,0]) <= int(rhalf[ir,0]):
-            arrayus=numpy.append(arrayus,[lhalf[il]],axis=0)
-            arrayus[counter,1] = row_track
+            array=numpy.append(array,[lhalf[il]],axis=0)
+            array[counter,1] = row_track
             row_track += 1
             counter += 1
             il += 1
 
         else:
-            arrayus= numpy.append(arrayus,[rhalf[ir]],axis=0)
-            arrayus[counter,1] = row_track
+            array= numpy.append(array,[rhalf[ir]],axis=0)
+            array[counter,1] = row_track
             row_track += 1
             counter += 1
             ir += 1
 
+
+        #check if merge completed
         if ir == len(rhalf):
+            #merge rest
             for i in range(len(lhalf)-il):
-                arrayus=numpy.append(arrayus,[lhalf[il+i]],axis=0)
-            while counter!= len(arrayus):
-                arrayus[counter,1] =row_track
+                array=numpy.append(array,[lhalf[il+i]],axis=0)
+            while counter!= len(array):
+                array[counter,1] =row_track
                 row_track += 1
                 counter += 1
             break
-
+        
         if il == len(lhalf):
             for i in range(len(rhalf)-ir):
-                arrayus=numpy.append(arrayus,[rhalf[ir+i]],axis=0)
-            while counter!= len(arrayus):
-                arrayus[counter,1] =row_track
+                array=numpy.append(array,[rhalf[ir+i]],axis=0)
+            while counter!= len(array):
+                array[counter,1] =row_track
                 row_track += 1
                 counter += 1
             break
     
-    #print(arrayus)
-    arrayus= numpy.delete(arrayus,0,axis=0)
-    #print(arrayus)
-    draw_all(arrayus)
-    return arrayus
+    #remove unnecessary element in array and draw array
+    array= numpy.delete(array,0,axis=0)
+    draw_all(array)
+    return array
 
-#make merge sort it should work recursively
+#merge sort splits array in two and calls itself again until smalles possible array
 def merge_sort(array):
     if len(array) < 2:
         draw_all(array)
         return array
 
     half = len(array) // 2
-    #draw_all(array)
     return merger(lhalf= (merge_sort(array[:half])),rhalf=(merge_sort(array[half:])))
 
 
-#create array with arrays in it to keep track of position
+#create array 2d array with value and position
 def CreateUnsortArray():
-    array= numpy.array([[random.randrange(1,500), i]for i in range(300)], dtype=int)
+    array= numpy.array([[random.randrange(1,500), i]for i in range(500)], dtype=int)
     print(array)
     return array
 
@@ -144,8 +152,11 @@ while run:
     while not_finished:
 
         array= CreateUnsortArray()
-        array=merge_sort(array)
-        draw_all(array)
+        array2= numpy.copy(array)
+        BumbleSortus(array)
+        screen.fill(white)
+        draw_all(array2)
+        merge_sort(array2)
         not_finished= False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
